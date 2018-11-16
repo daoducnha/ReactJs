a86c33fe7cabdc8fd993566908b2ad3bbf377ff7
ducnha <daoducnha29493@gmail.com>
2018-11-16 23:54:29 +0700

    Lap 2

--- reactjs.js
+++ reactjs.js
@@ -244,4 +244,44 @@ class Counter extends React.Component{
     return <button onClick = {this.clickHandler}>{this.state.count}</button>
   }
 }
+=================================
+
+
+class Details extends React.Component{
+  render(){
+    return (<h1>{this.props.details}</h1>)
+  }
+}
+
+class Button extends React.Component{
+  render(){
+    return (<button style= {{color: this.props.active ?"red" :"blue"}} onClick={() => this.props.clickHandler(this.props.id, this.props.name)}>{this.props.name}</button>)
+  }
+}
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {activeArr: [0,0,0,0], details:""}
+    this.clickHandler = this.clickHandler.bind(this)
+  }
+  clickHandler(id, details){
+    var arr = [0,0,0,0]
+    arr[id] = 1
+    this.setState({activeArr:arr,details:details})
+    console.log(id, details)
+  }
+  
+  render(){
+    return (
+      <div>
+        <Button id={0} active={this.state.activeArr[0]} clickHandler = {this.clickHandler} name = "one" />
+        <Button id={1} active={this.state.activeArr[1]} clickHandler = {this.clickHandler} name = "two"/>
+        <Button id={2} active={this.state.activeArr[2]} clickHandler = {this.clickHandler} name = "three"/>
+        <Button id={3} active={this.state.activeArr[3]} clickHandler = {this.clickHandler} name = "four"/>
+        <Details details = {this.state.details}/>
+      </div>
+    )
+  }
+}
+
 
